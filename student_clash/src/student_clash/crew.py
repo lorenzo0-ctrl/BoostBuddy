aaec443e78818812cabdb29b25af2b7e83f6db8d
LORENZO DE MARCO s299261 <s299261@studenti.polito.it>
2025-11-22 15:52:37 +0100

    v0.1.1

--- student_clash/src/student_clash/crew.py
+++ student_clash/src/student_clash/crew.py
@@ -2,63 +2,61 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
 @CrewBase
-class StudentClash():
-    """StudentClash crew"""
+class BalancedLifeCrew:
+    """BalancedLife crew"""
 
     agents: List[BaseAgent]
     tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
+    # leggi da agents.yaml
     @agent
-    def researcher(self) -> Agent:
+    def trainer_agent(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
+            config=self.agents_config['trainer_agent'], 
             verbose=True
         )
 
     @agent
-    def reporting_analyst(self) -> Agent:
+    def nutritionist_agent(self) -> Agent:
         return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            config=self.agents_config['nutritionist_agent'],
             verbose=True
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    @agent
+    def stress_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['stress_agent'],
+            verbose=True
+        )
+
+
+     # leggi da tasks.yaml
     @task
-    def research_task(self) -> Task:
+    def fitness_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+            config=self.tasks_config['fitness_task'],
         )
 
     @task
-    def reporting_task(self) -> Task:
+    def diet_task(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
+            config=self.tasks_config['diet_task'],
+        )
+
+    @task
+    def stress_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['stress_task'],
         )
 
     @crew
     def crew(self) -> Crew:
-        """Creates the StudentClash crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
-            verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            verbose=True
         )
